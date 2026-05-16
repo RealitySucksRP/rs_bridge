23e972c4cc31ea99e512d3c083bf71550a02aff2
RealitySucksRP <williamb1982@live.com>
2026-05-15 23:44:02 -0400

    v2.0.0 - ESX support, fuel bridge, locales, full adapter refactor

--- rs_bridge_full_v1.1.0/examples/client_example.lua
+++
@@ -1,6 +0,0 @@
-RegisterCommand('rsbridgeclient', function()
-    print('Framework:', exports.rs_bridge:GetFramework())
-    print('Job:', json.encode(exports.rs_bridge:GetJob()))
-
-    exports.rs_bridge:Notify('Client bridge test complete.', 'success')
-end)
